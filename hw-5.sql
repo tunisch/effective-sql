d4ce8be4cbd47a1d0a54d97b8795f213f4468524
Tunahan Turker Erturk <89503561+tunisch@users.noreply.github.com>
2026-02-24 17:46:21 +0300

    Add files via upload

---
+++ hw-5.sql
@@ -0,0 +1,28 @@
+/*
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+*/
+
+-- SELECT * FROM film
+-- WHERE title LIKE '%n'
+-- ORDER BY length DESC
+-- LIMIT 5;
+
+
+/*
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten filmler arasında, en kısa (length) olanlardan 6., 7., 8., 9. ve 10. sıradaki filmleri listele.
+*/
+
+-- SELECT title FROM film
+-- WHERE title LIKE '%n'
+-- ORDER BY length ASC
+-- OFFSET 5
+-- LIMIT 5;
+
+/*
+customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+*/
+
+SELECT last_name FROM customer 
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
